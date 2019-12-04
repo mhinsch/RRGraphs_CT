27f413686d0943e84fa2b6357f587ef8c5bc688f
Martin Hinsch <hinsch.martin@gmail.com>
2019-12-04 11:26:58 +0000

    documentation

--- Util/src/Pathfinding.jl
+++ Util/src/Pathfinding.jl
@@ -6,9 +6,15 @@ export path_Astar, path_costs, path_costs_estimate, each_neighbour
 using DataStructures
 
 
+# match either object == object or object in container depending on type
 @generated matches(current, target) = current == target ? :(current == target) : :(current in target)
 
 
+"Find the least-cost path from `start` to `target` using cost function
+`path_costs` and cost heuristic function `path_costs_estimate`. An iterator
+function to find a nodes' neighbours has to be provided in `each_neighbour`.
+Returns a vector `start`, node, ..., `target` and the number of iterations used
+to find the path in a tuple."
 function path_Astar(start, target, path_costs, path_costs_estimate, each_neighbour)
 	done = Set{typeof(start)}()
 
@@ -25,17 +31,21 @@ function path_Astar(start, target, path_costs, path_costs_estimate, each_neighbo
 	found = false
 
 	while length(known) > 0
-		#print("*")
+		# get the next node with the least (estimated) cost
 		current = dequeue!(known)
 
+		# we are already there
 		if matches(current, target)
 			found = true
 			break
 		end
 
+		# no need to check the current one again
 		push!(done, current)
 
+		# check all connected nodes
 		for c in each_neighbour(current)
+			# checked this one already
 			if c in done
 				continue
 			end
@@ -44,6 +54,7 @@ function path_Astar(start, target, path_costs, path_costs_estimate, each_neighbo
 
 			costs_thisway = costs_sofar[current] + path_costs(current, c)
 
+			# we might already know a shortcut to c
 			if haskey(costs_sofar, c) && costs_thisway > costs_sofar[c]
 				# no need to explore further since this path is obviously worse
 				continue
@@ -51,13 +62,12 @@ function path_Astar(start, target, path_costs, path_costs_estimate, each_neighbo
 
 			costs_sofar[c] = costs_thisway
 
+			# our best estimate for this path
 			known[c] = costs_thisway + path_costs_estimate(c, target)
 
 			previous[c] = current
 		end
 	end
-
-	#println()
 
 	path = Vector{typeof(start)}()
 
